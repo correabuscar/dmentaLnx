ea9c6d70c433a32b1a32532a1d9309e27c2ff510
correabuscar <correabuscar+githubcommits@gmail.com>
2022-10-22 20:51:28 +0200

    make tests pass and add license=

--- src/fifo_hashset/src/lib.rs
+++ src/fifo_hashset/src/lib.rs
@@ -13,46 +13,82 @@ mod tests {
     }
 
     #[test]
-    fn t2() {
+    fn kept_insertion_order_ints() {
+        //keeps insertion order as long as remove() is called on last item(s), or not called at
+        //all. See: https://github.com/japaric/heapless/issues/321
         use heapless::FnvIndexSet;
-        //use std::slice::range;
-        //use core::slice::range;
+        const LAST: usize = 128; //must be power of two, FnvIndexSet requirement
+        type Numbers = u32;
+        let mut nums = FnvIndexSet::<Numbers, LAST>::new();
+        const FIRST: Numbers = 1;
+        const THROWER: Numbers = 1000;
+        use std::ops::RangeInclusive;
+        //const DELETER:RangeInclusive<Numbers>=(FIRST+102)..=(LAST as Numbers - 5);
+        const DELETER: [RangeInclusive<Numbers>; 3] = [
+            (FIRST + 10)..=(FIRST + 25),
+            (FIRST + 50)..=(FIRST + 85),
+            (FIRST + 102)..=(LAST as Numbers - 5),
+        ];
+        const PICKER: Numbers = 100;
+        for i in FIRST..=LAST as Numbers {
+            let r: Result<bool, Numbers>;
+            r = if i == PICKER {
+                nums.insert(THROWER)
+            } else {
+                nums.insert(i)
+            };
+            //println!("{r:?}");
+            assert_eq!(r, Ok(true));
 
-        // src: https://japaric.github.io/heapless/heapless/struct.IndexSet.html
-        // A hash set with a capacity of 16 elements allocated on the stack
-        let mut books = FnvIndexSet::<_, 16>::new();
-
-        // Add some books.
-        books.insert("A Dance With Dragons").unwrap();
-        books.insert("To Kill a Mockingbird").unwrap();
-        books.insert("The Odyssey").unwrap();
-        books.insert("The Great Gatsby").unwrap();
-
-        /*let mut s:String;
-        for i in 0..=116+1 { //range(0,118) {
-            s=i.to_string();
-            books.insert(&s);
-        }*/
-
-        // Check for a specific one.
-        if !books.contains("The Winds of Winter") {
-            println!(
-                "We have {} books, but The Winds of Winter ain't one.",
-                books.len()
-            );
+            //having removal done here doesn't change order of existing items:
+            for all_d in DELETER {
+                if all_d.contains(&i) {
+                    let b = nums.remove(&i);
+                    assert_eq!(b, true);
+                }
+            }
         }
 
-        // Remove a book.
-        books.remove("The Odyssey");
+        //but having removal done here, does change the order of existing items:
+        /*for i in FIRST..=LAST as Numbers {
+            for all_d in DELETER {
+                if all_d.contains(&i) {
+                    let b = nums.remove(&i);
+                    assert_eq!(b, true);
+                }
+            }
+        }*/
 
-        // Iterate over everything.
-        for book in &books {
-            println!("{}", book);
+        /*for j in &nums {
+            println!("now: {j}");
+        }*/
+
+        let mut i: Numbers = FIRST;
+        let mut iter = nums.iter();
+        for j in &nums {
+            if let Some(cur) = iter.next() {
+                //println!("{:?}", cur);
+                if i == PICKER {
+                    assert_eq!(*cur, THROWER);
+                    assert_eq!(*j, THROWER);
+                } else {
+                    for all_d in DELETER {
+                        while all_d.contains(&i) {
+                            i += 1;
+                        }
+                    }
+                    assert_eq!(*j, i);
+                    assert_eq!(*cur, i);
+                }
+            }
+
+            i += 1;
         }
     }
 
     #[test]
-    fn kept_insertion_order_ints() { //it doesn't keep insertion order! (but wasn't clear, https://github.com/japaric/heapless/issues/321 )
+    fn doesnt_keep_insertion_order_after_remove_ints() {
+        //it doesn't keep insertion order! (but wasn't clear, https://github.com/japaric/heapless/issues/321 )
         use heapless::FnvIndexSet;
         const LAST: usize = 128; //must be power of two, FnvIndexSet requirement
         type Numbers = u32;
@@ -96,31 +132,50 @@ mod tests {
             }
         }
 
-        for j in &nums {
+        /*for j in &nums {
             println!("now: {j}");
-        }
+        }*/
 
         let mut i: Numbers = FIRST;
         let mut iter = nums.iter();
+        let mut diff = 0;
         for j in &nums {
             if let Some(cur) = iter.next() {
-                println!("{:?}", cur);
+                //println!("{:?}", cur);
                 if i == PICKER {
-                    assert_eq!(*cur, THROWER);
-                    assert_eq!(*j, THROWER);
+                    //assert_eq!(*cur, THROWER);
+                    //assert_eq!(*j, THROWER);
+                    #[allow(unused_parens)]
+                    let eq1 = (*j != THROWER);
+                    #[allow(unused_parens)]
+                    let eq2 = (*cur != THROWER);
+                    assert_eq!(eq1, eq2);
+                    if *j != THROWER {
+                        diff += 1;
+                    }
                 } else {
                     for all_d in DELETER {
                         while all_d.contains(&i) {
                             i += 1;
                         }
                     }
-                    assert_eq!(*j, i);
-                    assert_eq!(*cur, i);
+                    //assert_eq!(*j, i);
+                    #[allow(unused_parens)]
+                    let eq1 = (*j != i);
+                    #[allow(unused_parens)]
+                    let eq2 = (*cur != i);
+                    assert_eq!(eq1, eq2);
+                    #[allow(unused_parens)]
+                    if (*j != i) {
+                        diff += 1;
+                    }
+                    //assert_eq!(*cur, i);
                 }
             }
 
             i += 1;
         }
+        assert!(diff > 0);
     }
 
     /*#[test]
--- src/id/src/lib.rs
+++ src/id/src/lib.rs
@@ -1,3 +1,9 @@
+//Note: ID would be made out of a random number and its hash(eg. sha256) but salted hash, and the
+//salt would be a random per-database one, chosen when the database is first created!
+//
+//This would detect any potential corruption due to memory errors for example and also maybe tag
+//the IDs so they're known to come from a certain database.
+//
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
